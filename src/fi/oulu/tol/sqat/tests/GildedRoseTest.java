8201e14a1082dfdc8c5c327432000438f127fab2
maghaei <majid.aghaei1363@gmail.com>
2017-09-22 23:55:17 +0300

    Assignment finished

--- src/fi/oulu/tol/sqat/tests/GildedRoseTest.java
+++ src/fi/oulu/tol/sqat/tests/GildedRoseTest.java
@@ -21,22 +21,219 @@ public class GildedRoseTest {
 //   Item("Conjured Mana Cake", 3, 6));
 
 	@Test
-	public void testUpdateEndOfDay_AgedBrie_Quality_10_11() {
-		// Arrange
-		GildedRose store = new GildedRose();
-		store.addItem(new Item("Aged Brie", 2, 10) );
+	public void test_updateEndofDay_NewItem_Quality_10_9()
+	{
+		//Arrange
+		GildedRose gRose = new GildedRose();
+		gRose.addItem(new Item("New Item", 3, 10));
 		
-		// Act
-		store.updateEndOfDay();
+		//Act 
+		gRose.updateEndOfDay();
 		
-		// Assert
-		List<Item> items = store.getItems();
-		Item itemBrie = items.get(0);
-		assertEquals(11, itemBrie.getQuality());
+		List<Item> items = gRose.getItems();
+		Item item = items.get(0);
+		
+		//Assert
+		assertEquals("Quality should be reduced", 9, item.getQuality());
 	}
-    
+	
 	@Test
-	public void testUpdateEndOfDay() {
-		fail("Test not implemented");
+	public void test_updateEndofDay_NewItem_SellIn_3_2()
+	{
+		//Arrange
+		GildedRose gRose = new GildedRose();
+		gRose.addItem(new Item("New Item", 3, 10));
+		
+		//Act 
+		gRose.updateEndOfDay();
+		
+		List<Item> items = gRose.getItems();
+		Item item = items.get(0);
+		
+		//Assert
+		assertEquals("SellIn should be reduced", 2, item.getSellIn());
+	}
+	
+	@Test
+	public void test_updateEndofDay_NewItem_Quality_10_4()
+	{
+		//Arrange
+		GildedRose gRose = new GildedRose();
+		gRose.addItem(new Item("New Item", 2, 10));
+		
+		//Act 
+		gRose.updateEndOfDay();
+		gRose.updateEndOfDay();
+		gRose.updateEndOfDay();
+		gRose.updateEndOfDay();
+		
+		List<Item> items = gRose.getItems();
+		Item item = items.get(0);
+		
+		//Assert
+		assertEquals("Quality should be reduced by one until its sellin comes then reduced by two", 4, item.getQuality());
+	}
+	
+	@Test
+	public void test_updateEndofDay_NewItem_Quality_3_0()
+	{
+		//Arrange
+		GildedRose gRose = new GildedRose();
+		gRose.addItem(new Item("New Item", 2, 3));
+		
+		//Act 
+		gRose.updateEndOfDay();
+		gRose.updateEndOfDay();
+		gRose.updateEndOfDay();
+		gRose.updateEndOfDay();
+		
+		List<Item> items = gRose.getItems();
+		Item item = items.get(0);
+		
+		//Assert
+		assertEquals("Quality should not be negative", 0, item.getQuality());
+	}
+	
+	@Test
+	public void test_updateEndofDay_AgedBrie_Quality_5_6()
+	{
+		//Arrange
+		GildedRose gRose = new GildedRose();
+		gRose.addItem(new Item("Aged Brie", 3, 5));
+		
+		//Act 
+		gRose.updateEndOfDay();
+		
+		List<Item> items = gRose.getItems();
+		Item agedBrie = items.get(0);
+		
+		//Assert
+		assertEquals("Aged Brie quality should be increased", 6, agedBrie.getQuality());
+	}
+	
+	@Test
+	public void test_updateEndofDay_AgedBrie_SellIn_3_2()
+	{
+		//Arrange
+		GildedRose gRose = new GildedRose();
+		gRose.addItem(new Item("Aged Brie", 3, 5));
+		
+		//Act 
+		gRose.updateEndOfDay();
+		List<Item> items = gRose.getItems();
+		Item agedBrie = items.get(0);
+		
+		//Assert
+		assertEquals("Aged Brie sellIn should be reduced", 2, agedBrie.getSellIn());
+	}
+	
+	@Test
+	public void test_updateEndofDay_Sulfuras_SellIn_3_3()
+	{
+		//Arrange
+		GildedRose gRose = new GildedRose();
+		gRose.addItem(new Item("Sulfuras, Hand of Ragnaros", 3, 5));
+		
+		//Act 
+		gRose.updateEndOfDay();
+		List<Item> items = gRose.getItems();
+		Item item = items.get(0);
+		
+		//Assert
+		assertEquals("Sulfuras SellIn should not be reduced", 3, item.getSellIn());
+	}
+	
+	@Test
+	public void test_updateEndofDay_Sulfuras_Quality_80_80()
+	{
+		//Arrange
+		GildedRose gRose = new GildedRose();
+		gRose.addItem(new Item("Sulfuras, Hand of Ragnaros", 3, 80));
+		
+		//Act 
+		gRose.updateEndOfDay();
+		gRose.updateEndOfDay();
+		
+		List<Item> items = gRose.getItems();
+		Item item = items.get(0);
+		
+		//Assert
+		assertEquals("Sulfuras quality will never change", 80, item.getQuality());
+	}
+		
+	@Test
+	public void test_updateEndofDay_Backstage_Quality_15_17()
+	{
+		//Arrange
+		GildedRose gRose = new GildedRose();
+		gRose.addItem(new Item("Backstage passes to a TAFKAL80ETC concert", 7, 15));
+		
+		//Act 
+		gRose.updateEndOfDay();
+		
+		List<Item> items = gRose.getItems();
+		Item item = items.get(0);
+		
+		//Assert
+		assertEquals("The selling day is less than 10 and more than 5, so quality should be increased by two", 17, item.getQuality());
+	}
+	
+	@Test
+	public void test_updateEndofDay_Backstage_Quality_10_13()
+	{
+		//Arrange
+		GildedRose gRose = new GildedRose();
+		gRose.addItem(new Item("Backstage passes to a TAFKAL80ETC concert", 4, 10));
+		
+		//Act 
+		gRose.updateEndOfDay();
+		
+		List<Item> items = gRose.getItems();
+		Item item = items.get(0);
+		
+		//Assert
+		assertEquals("The selling day is less than 5, so quality should be increased by three", 13, item.getQuality());
+	}
+	
+	@Test
+	public void test_updateEndofDay_Backstage_Quality_15_0()
+	{
+		//Arrange
+		GildedRose gRose = new GildedRose();
+		gRose.addItem(new Item("Backstage passes to a TAFKAL80ETC concert", 4, 15));
+		
+		//Act 
+		gRose.updateEndOfDay();
+		gRose.updateEndOfDay();
+		gRose.updateEndOfDay();
+		gRose.updateEndOfDay();
+		gRose.updateEndOfDay();
+		
+		List<Item> items = gRose.getItems();
+		Item item = items.get(0);
+		
+		//Assert
+		assertEquals("Quality should drop to zero since the concert day has arrived", 0, item.getQuality());
+	}
+	
+	@Test
+	public void test_updateEndofDay_AgedBrie_Quality_45_50()
+	{
+		//Arrange
+		GildedRose gRose = new GildedRose();
+		gRose.addItem(new Item("Aged Brie", 8, 45));
+		
+		//Act 
+		gRose.updateEndOfDay();
+		gRose.updateEndOfDay();
+		gRose.updateEndOfDay();
+		gRose.updateEndOfDay();
+		gRose.updateEndOfDay();
+		
+		List<Item> items = gRose.getItems();
+		Item item = items.get(0);
+		
+		//Assert
+		assertEquals("Quality should not be more than 50", 50, item.getQuality());
 	}
 }
